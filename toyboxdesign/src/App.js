7bbd8650b6d312cee12d56f218b5c64f0e59a60b
Ian Toy <iet@ela.com>
2020-02-11 17:31:59 -0800

    added starter footer, installed react-router-dom, began work on pages

--- toyboxdesign/src/App.js
+++ toyboxdesign/src/App.js
@@ -1,11 +1,26 @@
 import React from 'react';
+
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+import Home from './pages/Home';
 
 function App() {
   return (
-    <Navbar />
+    <Router>
+      <div>
+        <Navbar />
+
+          <Route exact path="/" component= {Home} />
+
+        <Footer />
+      </div>
+    </Router>
+
+
   );
 }
 
