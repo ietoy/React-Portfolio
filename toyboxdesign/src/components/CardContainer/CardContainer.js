9ead0aa5caaaaab7fd933930462b3913fd4cbcc0
Ian Toy <iet@ela.com>
2020-02-13 17:44:25 -0800

    styled header, picture, and contact info on contact page. Add review carousel later

---
+++ toyboxdesign/src/components/CardContainer/CardContainer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function CardContainer(props) {
+    return (
+        <div className="container row" {...props} />
+    )
+}
+
+export default CardContainer;
--- toyboxdesign/src/pages/Contact.js
+++ toyboxdesign/src/pages/Contact.js
@@ -3,18 +3,27 @@ import React from "react";
 function Contact() {
     return (
         <div className="container">
-            <h1>Contact</h1>
-            <p>Please feel free to contact me through any of the below means. I look forward to hearing from you!</p>
+            <h1 className="center">Contact</h1>
 
-            <img className="" src="https://ietoy.github.io/professional-portfolio/images/profile-pic.jpg" />
+            <div className="row">
+                <div className="col m6 s12">
+                    <div className="center">
+                        <img className="circle" src="https://ietoy.github.io/professional-portfolio/images/profile-pic.jpg" />
 
-            <p>Email: iantoy3235@gmail.com</p>
-            <p>Phone: (925) 667-6453</p>
-            <p>Link to my <a href="https://github.com/ietoy" target="_blank">GitHub</a></p>
-            <p>Link to my <a href="https://www.linkedin.com/in/ian-toy-265077196/" target="_blank">LinkedIn</a></p>
+                    </div>
+                </div>
 
-            {/* GET HELP HERE */}
-            <p>For more detailed work history, here is my <a href="assets/IET-resume.pdf" target="_blank">resume</a></p>
+                <div className="col m6 s12">
+                    <p>Email: iantoy3235@gmail.com</p>
+                    <p>Phone: (925) 667-6453</p>
+                    <p>Link to my <a href="https://github.com/ietoy" target="_blank">GitHub</a></p>
+                    <p>Link to my <a href="https://www.linkedin.com/in/ian-toy-265077196/" target="_blank">LinkedIn</a></p>
+                    <p>For more detailed work history, here is my <a href="./assets/IET-resume.pdf" target="_blank">resume</a></p>
+                </div>
+
+            </div>
+
+
 
         </div>
     )
